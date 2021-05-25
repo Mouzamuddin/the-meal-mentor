beea3130a6e0185b8488e92fe2b63f82480ee18d
Mouzamuddin Mir <82389773+Mouzamuddin@users.noreply.github.com>
2021-05-25 21:50:57 +0530

    updated style

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ const cuisineDB =
 {
   Indiacuisine: 
   [
-    {name: "Murgh Makhani (Butter Chicken)",rating: "4/5" },
-    { name: "Rogan Josh (Red Lamb)", rating: "5/5" },
-    { name: "Malai Kofta (veg)", rating: "3.5/5" },
-    { name: "Dum ki Biryani", rating: "5/5" }
+    {name: "Murgh Makhani (Butter Chicken)",rating: "4/5⭐" },
+    { name: "Rogan Josh (Red Lamb)", rating: "5/5⭐" },
+    { name: "Malai Kofta (veg)", rating: "3.5/5⭐" },
+    { name: "Dum ki Biryani", rating: "5/5⭐" }
   ],
   arabiancuisine: [
-    { name: "Hummus", rating: "4/5" },
-    { name: "Shawarma", rating: "5/5" },
-    { name: "Shish tawook", rating: "4.5/5" },
-    { name: "Baklava", rating: "3.5/5" },
-    { name: "Knafeh", rating: "5/5" }
+    { name: "Hummus", rating: "4/5⭐" },
+    { name: "Shawarma", rating: "5/5⭐" },
+    { name: "Shish tawook", rating: "4.5/5⭐" },
+    { name: "Baklava", rating: "3.5/5⭐" },
+    { name: "Knafeh", rating: "5/5⭐" }
   ], 
   italiancuisine: [
-    { name: "Pizza", rating: "5/5" },
-    { name: "Lasagna", rating: "3.5/5" },
-    { name: "Tiramisu", rating: "4.5/5" },
+    { name: "Pizza", rating: "5/5⭐" },
+    { name: "Lasagna", rating: "3.5/5⭐" },
+    { name: "Tiramisu", rating: "4.5/5⭐" },
   ],
 };
 
